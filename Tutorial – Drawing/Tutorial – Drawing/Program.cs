76f80456eddd0bd5483ecbcc519d585681a19fed
bradly0cjw <57744542+bradly0cjw@users.noreply.github.com>
2024-10-24 21:08:04 +0800

    [upload] Lab2

---
+++ Tutorial – Drawing/Tutorial – Drawing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+namespace Tutorial___Drawing
+{
+    class MainEntry
+    {
+        static void Main(string[] args)
+        {
+            Form form = new ElfinForm();
+            Application.Run(form);
+        }
+    }
+}
